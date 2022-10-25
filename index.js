8a352080f8f12a81532315018a8dea8a793e3b81
Ahmad Hariri <ahmadmhd357@gmail.com>
2022-10-26 00:36:10 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body:JSON.stringify({
+//         name: userName,
+//         email: userEmail,
+//       })
+//   };
+let body = document.querySelector('body');
+function submitData(userName,userEmail){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body:JSON.stringify({
+            name: userName,
+            email: userEmail,
+          })
+           }
+        )
+    .then(function (response) {
+    return response.json();
+      })
+    .then(function (object) {
+    body.append(object.id)
+      })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+    body.append(error.message);
+      });
+      
+}
+// submitData('ahmad','ahmadmhd357@gmail.com')
